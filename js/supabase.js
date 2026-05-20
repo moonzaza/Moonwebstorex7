de00087bec354206e771be1dfcf98bf0139ed0af
moonzaza <z4702988@gmail.com>
2026-05-19 23:20:09 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // EDITAR AQUÍ CON TUS CREDENCIALES DE SUPABASE
-const SUPABASE_URL = 'https://qjkitiblzilzlbhcdwis.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqa2l0aWJsemlsemxiaGNkd2lzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMDQzNjksImV4cCI6MjA5NDc4MDM2OX0.0HsFl5v9IpqBqB5yv0R25JiCsydcCv1JL58RRHL5ros';
+const SUPABASE_URL = 'https://qrvpycpdrcgymzybdrby.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFydnB5Y3BkcmNneW16eWJkcmJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyNDQwMTYsImV4cCI6MjA5NDgyMDAxNn0.2EI0vonSZuTvNXgsPNl8AM36WdAZh2Xi186-24JDk6g';
 
 // Inicializar cliente de Supabase
 const { createClient } = supabase;
